2fbb38f941b3f2ea94cadd7d298310e4a53ddd3b
Harshil Tanwar <harshiltanwar62@gmail.com>
2024-11-13 20:05:07 +0530

    style: home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import AppLayout from '../components/layout/AppLayout';
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center h-screen text-center p-8 w-2/3">
+    <div className="flex items-center justify-center h-screen text-center p-8 w-full">
       <div className="space-y-4">
         <h1 className="text-3xl font-bold text-teal-700">Welcome to Yapper!</h1>
         <p className="text-lg text-teal-600">Click on a chat to start a conversation.</p>
